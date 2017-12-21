453ec74ceada55bfc78f487abe89f567ab43de1e
Nelson Dornelas <ndornelasj@gmail.com>
2017-12-20 17:30:53 -0800

    Finish simple static_pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,29 +2,23 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
-  describe "GET #home" do
-    it "returns http success" do
+  describe 'GET static_pages' do
+    it '#home returns http success' do
       get :home
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe "GET #help" do
-    it "returns http success" do
+    it '#help returns http success' do
       get :help
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe "GET #about" do
-    it "returns http success" do
+    it '#about returns http success' do
       get :about
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe "GET #contact" do
-    it "returns http success" do
+    it '#contact returns http success' do
       get :contact
       expect(response).to have_http_status(:success)
     end
