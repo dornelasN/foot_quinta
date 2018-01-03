865188b02b02a418dd3cd24588ec31cfd1cc3fb3
Nelson Dornelas <ndornelasj@gmail.com>
2018-01-03 12:05:23 -0800

    login and logout: remember and forget cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # Schema: User(name:string, email:string, password_digest:string)
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { downcase_email }
 
   # Secure Password Attribute (password and password_confirmation)
@@ -19,6 +20,35 @@ class User < ApplicationRecord
   # Profile picture attribute
   mount_uploader :picture, ProfilePictureUploader
   validate :picture_size
+
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token
+  def self.new_token
+    # Returns a random string of length 22
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 
   private
 
